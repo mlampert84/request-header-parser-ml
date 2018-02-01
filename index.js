5d20ff2b1d2a46997f60beefef64e8d3557bde65
mlampert84 <marcus.lampert@gmail.com>
2018-02-01 11:58:07 +0100

    initial commit of app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+
+express()
+   .get('/api/whoami', (req,res) => {
+     var whoami  = { ipaddress: req.ip,
+                     language: req.headers.accept-language,
+                     software: req.user-agent}
+
+     
+     console.log(JSON.stringify(req.headers))
+     res.send(JSON.stringify(whoami))
+   })
+     .listen(PORT, () => console.log('Listening on PORT: ' + PORT))
+   
