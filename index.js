36b4b90dcf9bc87a88b4fc25e05b3ecfc7c782c5
mlampert84 <marcus.lampert@gmail.com>
2018-02-06 17:24:38 +0100

    fixing user agent output

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 var languageParser = require('accept-language-parser')
 
+
 express()
    .get('/api/whoami', (req,res) => {
 
     var languages = languageParser.parse(req.headers['accept-language'])
+    var userAgent = req.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+
+
 
      var whoami  = { ipaddress: req.headers['x-forwarded-for'] || req.ip,
                      language: languages[0].code + '-' + languages[0].region,
-                     software: req.headers['user-agent']}
-
+                     software: userAgent
+     }
      
      console.log(JSON.stringify(req.headers))
      res.send(JSON.stringify(whoami))
