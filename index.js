3239be9ebbd3822e0bcd9b9a459a6b2ad8ff4e03
mlampert84 <marcus.lampert@gmail.com>
2018-02-06 16:42:10 +0100

    Add language parser. Only return first language result

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
-
+var languageParser = require('accept-language-parser')
 
 express()
    .get('/api/whoami', (req,res) => {
+
+    var languages = languageParser.parse(req.headers['accept-language'])
+
      var whoami  = { ipaddress: req.headers['x-forwarded-for'] || req.ip,
-                     language: req.headers['accept-language'],
+                     language: languages[0].code + '-' + languages[0].region,
                      software: req.headers['user-agent']}
 
      
